3460ff35ae396283c20ec3588d01408e3fad8fcc
jasmaljabbar <jasmaljabbar24@gmail.com>
2023-12-19 13:37:15 +0530

    changed the form message

--- assets/js/main.js
+++ assets/js/main.js
@@ -333,7 +333,7 @@
   form.addEventListener('submit', e => {
     e.preventDefault()
     fetch(scriptURL, { method: 'POST',body: new FormData(form)})
-    .then(response => alert("Thank you ! your form is submitted  submitted successfully."))
+    .then(response => alert("Thank you ! your form is submitted successfully."))
     .then(() => { window.location.reload(); })
     .catch(error => console.error('Error!',error.message))
   })
